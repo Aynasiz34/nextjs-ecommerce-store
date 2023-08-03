4dc091c1c76cb31450b10dd1d198f19885d9846d
Aynasiz34 <gomserim@gmail.com>
2023-08-03 15:03:26 +0300

    Featured Products page created

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
 
-const HomePage = () => {
-  return <div>Home Page</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("0407683d-449b-47eb-8077-8d5df87efc7c");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
